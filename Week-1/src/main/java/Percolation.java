df128e1be7f83079f21b6fbd9238aea9cc435c86
Alex Gascón <alexgascon.93@gmail.com>
2019-08-12 22:11:08 +0100

    Implementing isFull method
    Lots of changes here:
    - Creating the method convertToGrid to be able to obtain the position of
    a row/col element in the grid object
    - Adding the concept of TOP and BOTTOM element. We'll use them to know
    if any point of the top row is connected to any point of the bottom row
    - Creating the connect method, to connect two points (each represented
    by a row+col combination). Here is where we set the logic to avoid
    connecting to one point if it's closed or out of bounds.
    - Creating the method connectFirstRowToTop, to make sure that all the
    elements in the first row are connected to the top of the grid
    - Creating the method connectToNeighbors, to simplify the connection of
    one element to its neighbors when we open it
    - Refactoring the open method: we have divided the logic into two helper
    methods, one to handle the opening of the site (i.e. putting the tile to
    true + increasing the count) and another to connecting to the
    neighboring elements
    - Finally, implementing isFull: we consider that an element is full if
    it is connected to the top element

--- Week-1/src/main/java/Percolation.java
+++ Week-1/src/main/java/Percolation.java
@@ -5,30 +5,34 @@ public class Percolation {
     WeightedQuickUnionUF grid;
     boolean[][] openSites;
 
+    static final int TOP_ELEMENT = 0;
+
     // Creates an n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         this.n = n;
-        this.grid = new WeightedQuickUnionUF(n * n);
+        this.grid = new WeightedQuickUnionUF(n * n + 2);
         this.openSites = new boolean[n][n];
         this.openSitesCount = 0;
+
+        connectFirstRowToTop();
     }
 
     // Opens the site (row, col) if it's not open
     public void open(int row, int col) {
         if (isOpen(row, col)) return;
 
-        openSitesCount++;
-        openSites[row][col] = true;
+        openSite(row, col);
+        connectToNeighbors(row, col);
     }
 
     // Is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return openSites[row][col];
+        return openSites[row - 1][col - 1];
     }
 
     // Is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return false;
+        return grid.connected(TOP_ELEMENT, convertToGrid(row, col));
     }
 
     // Returns the number of open sites
@@ -43,5 +47,38 @@ public class Percolation {
 
     // test client (optional)
     public static void main(String[] args) {
+    }
+
+    private int convertToGrid(int row, int col) {
+        return (row - 1) * n + col;
+    }
+
+    private void openSite(int row, int col) {
+        openSitesCount++;
+        openSites[row - 1][col - 1] = true;
+    }
+
+    private void connectFirstRowToTop() {
+        for (int col = 1; col <= n; col++)
+            grid.union(TOP_ELEMENT, convertToGrid(1, col));
+    }
+
+    private void connectToNeighbors(int row, int col) {
+        connect(row, col, row - 1, col); // Top
+        connect(row, col, row + 1, col); // Bottom
+        connect(row, col, row, col - 1); // Left
+        connect(row, col, row, col + 1); // Right
+    }
+
+    private void connect(int row1, int col1, int row2, int col2) {
+        // Avoid the connection if the neighbor is out of bounds
+        if ((row2 < 1) || (row2 > n) || (col2 < 1) || (col2 > n)) return;
+
+        // Avoid the connection if the neighbor is closed
+        if (!isOpen(row2, col2)) return;
+
+        int element1 = convertToGrid(row1, col1);
+        int element2 = convertToGrid(row2, col2);
+        grid.union(element1, element2);
     }
 }
