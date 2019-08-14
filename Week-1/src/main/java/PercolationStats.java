bc6829b1a0ecb67e509172c77354c6c43516e21d
Alex Gascón <alexgascon.93@gmail.com>
2019-08-14 22:19:08 +0100

    Running Percolation experiments + creating client
    Modifying PercolationStats' constructor to make it run the specified
    number of experiments and store its results
    
    Also, creating a client in the main() method that follows the assignment
    instructions
    
    NOTE: We've set most of the fields of the class as private. This is
    required by the assignment autograder, or otherwise it determines that
    the API doesn't match the specified one (because it has extra fields)
    and doesn't even grade the assignment. However, this will cause the
    tests to fail, as they were setting the experiment results manually

--- Week-1/src/main/java/PercolationStats.java
+++ Week-1/src/main/java/PercolationStats.java
@@ -1,17 +1,37 @@
 import java.lang.Math;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-    final int n, trials, totalSites;
-    double[] proportionOfSitesOpenedPerTrial;
+    private final int n, trials;
+    private final double totalSites;
+    private final double[] proportionOfSitesOpenedPerTrial;
 
     // Perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) throw new java.lang.IllegalArgumentException("ERROR");
+
         this.n = n;
         this.trials = trials;
-        this.totalSites = n * n;
+        this.totalSites = (double) n * n;
+        this.proportionOfSitesOpenedPerTrial = new double[trials];
+
+        for (int trial = 0; trial < trials; trial++) {
+            Percolation percolation = new Percolation(n);
+
+            while (true) {
+                int randomRow = StdRandom.uniform(1, n + 1);
+                int randomCol = StdRandom.uniform(1, n + 1);
+                percolation.open(randomRow, randomCol);
+
+                if (percolation.percolates()) break;
+            }
+
+            this.proportionOfSitesOpenedPerTrial[trial] = percolation.numberOfOpenSites() / totalSites;
+        }
     }
 
     // Sample mean of percolation threshold
@@ -32,5 +52,15 @@ public class PercolationStats {
     // High endpoint of 95% confidence interval
     public double confidenceHi() {
         return mean() + 1.96*stddev()/Math.sqrt(trials);
+    }
+
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        int trials = StdIn.readInt();
+
+        PercolationStats stats = new PercolationStats(n, trials);
+        StdOut.print("mean = " + stats.mean());
+        StdOut.print("stddev = " + stats.stddev());
+        StdOut.print("95% confidence interval = [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
     }
 }
