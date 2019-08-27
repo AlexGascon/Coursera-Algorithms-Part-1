f97c655cc0ec74ab5b5b2dab3e7d943928661eca
Alex Gascón <alexgascon.93@gmail.com>
2019-08-27 21:55:38 +0100

    Style fixes
    - Substituting all the tabs for spaces
    - Avoid using the full path for java.lang.IllegalArgumentException
    (classes in java.lang) are automatically imported

--- Week-3/BruteCollinearPoints.java
+++ Week-3/BruteCollinearPoints.java
@@ -2,65 +2,65 @@ import java.util.Arrays;
 
 public class BruteCollinearPoints {
 
-	private int num_segments = 0;
-	private LineSegment[] found_segments;
+    private int num_segments = 0;
+    private LineSegment[] found_segments;
 
-	public BruteCollinearPoints(Point[] points) {
-		validateInput(points);
+    public BruteCollinearPoints(Point[] points) {
+        validateInput(points);
 
-		num_segments = 0;
-		found_segments = new LineSegment[points.length];
+        num_segments = 0;
+        found_segments = new LineSegment[points.length];
 
-		for (int i = 0; i < points.length; i++) 
-			for (int j = i+1; j < points.length; j++)
-				for (int k = j+1; k < points.length; k++) {
-					
-					// Checking if the first 3 points are collinear
-					Point p = points[i];
-					Point q = points[j];
-					Point r = points[k];
+        for (int i = 0; i < points.length; i++)
+            for (int j = i+1; j < points.length; j++)
+                for (int k = j+1; k < points.length; k++) {
 
-					if (p.slopeTo(q) == p.slopeTo(r))
+                    // Checking if the first 3 points are collinear
+                    Point p = points[i];
+                    Point q = points[j];
+                    Point r = points[k];
 
-						// If they are collinear, we look for a forth one
-						for (int l = k+1; l < points.length; l++) {
-							Point s = points[l];
-							if (p.slopeTo(s) == p.slopeTo(q)) {
-								addSegment(p, q, r, s);
-							} 
-						}
+                    if (p.slopeTo(q) == p.slopeTo(r))
 
-				}
+                        // If they are collinear, we look for a forth one
+                        for (int l = k+1; l < points.length; l++) {
+                            Point s = points[l];
+                            if (p.slopeTo(s) == p.slopeTo(q)) {
+                                addSegment(p, q, r, s);
+                            }
+                        }
 
-	}
-	public int numberOfSegments() { return num_segments; }
-	public LineSegment[] segments() {
-		return Arrays.copyOfRange(found_segments, 0, numberOfSegments());
-	}
+                }
 
-	private void addSegment(Point p, Point q, Point r, Point s) {
-		
-		// Finding the most separated set of points
-		Point[] collinear_points = {p, q, r, s};
-		Arrays.sort(collinear_points);
-		Point min_point = collinear_points[0];
-		Point max_point = collinear_points[3];
-		
-		found_segments[num_segments++] = new LineSegment(min_point, max_point);
-	}
+    }
+    public int numberOfSegments() { return num_segments; }
+    public LineSegment[] segments() {
+        return Arrays.copyOfRange(found_segments, 0, numberOfSegments());
+    }
 
-	public static void main(String[] args) {
-		// Empty method
-	}
+    private void addSegment(Point p, Point q, Point r, Point s) {
 
-	private void validateInput(Point[] points) {
-		if (points == null) throw new java.lang.IllegalArgumentException("The input argument is null");
-		for (point : points) if (point == null) throw new java.lang.IllegalArgumentException("The input contains null points");
+        // Finding the most separated set of points
+        Point[] collinear_points = {p, q, r, s};
+        Arrays.sort(collinear_points);
+        Point min_point = collinear_points[0];
+        Point max_point = collinear_points[3];
 
-		for (int i = 0; i < points.length; i++)
-			for (int j = i+1; j < points.length; j++)
-				if (points[i].compareTo(points[j]) == 0)
-					throw new java.lang.IllegalArgumentException("The input contains repeated points");
+        found_segments[num_segments++] = new LineSegment(min_point, max_point);
+    }
 
-	}
+    public static void main(String[] args) {
+        // Empty method
+    }
+
+    private void validateInput(Point[] points) {
+        if (points == null) throw new IllegalArgumentException("The input argument is null");
+        for (Point point : points) if (point == null) throw new IllegalArgumentException("The input contains null points");
+
+        for (int i = 0; i < points.length; i++)
+            for (int j = i+1; j < points.length; j++)
+                if (points[i].compareTo(points[j]) == 0)
+                    throw new IllegalArgumentException("The input contains repeated points");
+
+    }
 }
