5c43e76dffcbcdfb6942975a292c9a60947ab1ee
Alex Gascón <alexgascon.93@gmail.com>
2019-08-31 17:12:38 +0100

    Starting from scratch
    We seemed to be very far from the solution, so we'll start from scratch
    to avoid being biased from previous approaches
    
    For the moment, we've implemented the API and the input validation

--- Week-3/FastCollinearPoints.java
+++ Week-3/FastCollinearPoints.java
@@ -1,86 +1,24 @@
-import java.util.Arrays;
-
 public class FastCollinearPoints {
-
-    private int numSegments = 0;
-    private LineSegment[] foundSegments;
-    private Point[] points, currentPoints, minPoints, maxPoints;
 
     public FastCollinearPoints(Point[] points) {
         validateInput(points);
-
-        this.points = Arrays.copyOfRange(points, 0, points.length);
-        minPoints = new Point[points.length];
-        maxPoints = new Point[points.length];
-
-        for (Point p : points) {
-            Point[] slopesToP = Arrays.copyOfRange(points, 0, points.length);
-            Arrays.sort(slopesToP, p.slopeOrder());
-
-            // We start the inner loop in 1 because the element with less slope 
-            // will be the point itself (which has -Infinity)
-            for (int j = 1; j < slopesToP.length - 2; j++) {
-                if (p.slopeTo(slopesToP[j]) == p.slopeTo(slopesToP[j+1]))
-                    // We avoid this comparison if the first two points aren't collinear
-                    if (p.slopeTo(slopesToP[j]) == p.slopeTo(slopesToP[j+2]))
-                        addToSegment(p, slopesToP[j], slopesToP[j+1], slopesToP[j+2]);
-            }
-        }
-
     }
-    public int numberOfSegments() { return numSegments; }
+
+    public int numberOfSegments() {
+        return 0;
+    }
 
     public LineSegment[] segments() {
-        foundSegments = new LineSegment[numberOfSegments()];
-
-        for (int i = 0; i < foundSegments.length; i++)
-            foundSegments[i] = new LineSegment(minPoints[i], maxPoints[i]);
-
-        return Arrays.copyOfRange(foundSegments, 0, numberOfSegments());
-    }
-
-    private void addToSegment(Point p, Point q, Point r, Point s) {
-        // Finding the most separated set of points
-        Point[] collinearPoints = {p, q, r, s};
-        Arrays.sort(collinearPoints);
-        Point minPoint = collinearPoints[0];
-        Point maxPoint = collinearPoints[3];
-
-        for (int i = 0; i < numSegments; i++) {
-            if (minPoints[i].compareTo(minPoint) == 0 && maxPoints[i].compareTo(maxPoint) == 0)
-                return;
-        }
-
-        if (numSegments == minPoints.length) {
-            resizePoints(minPoints.length * 2);
-        }
-
-        minPoints[numSegments] = minPoint;
-        maxPoints[numSegments] = maxPoint;
-        numSegments++;
-    }
-
-    private void resizePoints(int capacity) {
-        Point[] minCopy = new Point[capacity];
-        Point[] maxCopy = new Point[capacity];
-
-        for (int i = 0; i < minPoints.length; i++) {
-            minCopy[i] = minPoints[i];
-            maxCopy[i] = maxPoints[i];
-        }
-
-        this.minPoints = minCopy;
-        this.maxPoints = maxCopy;
+        return new LineSegment[0];
     }
 
     private void validateInput(Point[] points) {
-        if (points == null) throw new IllegalArgumentException("The input argument is null");
+        if (points == null) throw new IllegalArgumentException("The input cannot be null");
 
-        for (Point point : points) if (point == null) throw new IllegalArgumentException("The input contains null points");
+        for (Point point : points) if (point == null) throw new IllegalArgumentException("The input cannot contain null points");
 
         for (int i = 0; i < points.length; i++)
-            for (int j = i+1; j < points.length; j++)
-                if (points[i].compareTo(points[j]) == 0)
-                    throw new IllegalArgumentException("The input contains repeated points");
+            for (int j = i + 1; j < points.length; j++)
+                if (points[i] == points[j]) throw new IllegalArgumentException("The input cannot contain repeated points");
     }
 }
